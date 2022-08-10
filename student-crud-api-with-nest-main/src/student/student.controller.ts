4bebb3a52b5323a075a39c2950a43f428081cc28
Akashay Hosurkar <akashay.hosurkar@olacabs.com>
2022-08-10 11:49:25 +0530

    user folder is created

---
+++ student-crud-api-with-nest-main/src/student/student.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { StudentService } from './student.service';
+@Controller('student')
+export class StudentController {
+  constructor(private studentService: StudentService) {}
+  @Get()
+  getAllStudents() {
+    return this.studentService.getStudents();
+  }
+  @Get(':index')
+  getSpecificStudent(@Param() index) {
+    console.log(index);
+    return this.studentService.getSpecificStudent(index.index);
+  }
+  @Post()
+  createStudent(@Body() student) {
+    return this.studentService.createStudent(student);
+  }
+  @Delete(':id')
+  deleteStudent(@Param() id) {
+    return this.studentService.deleteStudent(id);
+  }
+  @Patch(':id')
+  updateStudent(@Param() id, @Body() student) {
+    return this.studentService.updateStudent(id.id, student);
+  }
+}
